edd3e75ff53b90269d947a634efc1d8df30e3fb4
AdamCollins <adamcollinssecure@gmail.com>
2019-06-17 01:13:27 -0700

    added laoshi(front-end)

---
+++ laoshi/precache-manifest.3e6117b6a1cba47c8b716434950ab595.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85d529762dcbc3966519266d7652cbcd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c06a3b161db881c7a5ed",
+    "url": "/static/css/main.27749051.chunk.css"
+  },
+  {
+    "revision": "33d4f3a44278136a03aa",
+    "url": "/static/js/2.fe74e7a7.chunk.js"
+  },
+  {
+    "revision": "c06a3b161db881c7a5ed",
+    "url": "/static/js/main.85a47618.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
